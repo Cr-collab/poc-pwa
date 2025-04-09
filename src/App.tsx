42c94dc6cc9814d84a24769c2a1dbc04d4bf5e3e
Cr-collab <cristiccorrea@gmail.com>
2025-04-09 17:29:02 -0300

    test 4

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,37 @@
-import {  useState } from "react";
+import { useState } from "react";
 import "./App.css";
 import CameraPWA from "./pages/Camera.tsx";
-import { Test } from "./pages/QrScanner.tsx";
+import Html5QrcodePlugin from "./pages/QrScanner.tsx";
 import { GalleryAccess } from "./pages/TakeTheGalery.tsx";
 
 function App() {
-  const [isShowQrCode, setIsShowQrCode] = useState(false); 
+  const [isShowQrCode, setIsShowQrCode] = useState(false);
+  const [qrCode, setQrCode] = useState('Não foi lido');
+    // eslint-disable-next-line
+    // @ts-ignore
+  const onNewScanResult = (decodedText, decodedResult) => {
+    console.log("🚀 ~ onNewScanResult ~ decodedResult:", decodedResult)
+    console.log("🚀 ~ onNewScanResult ~ decodedText:", decodedText)
+    setQrCode(decodedResult)
+    // handle decoded results here
+};
 
   return (
     <>
       <GalleryAccess />
-      <button onClick={() => setIsShowQrCode(!isShowQrCode)}> Mostre QrCode</button>
-      {!isShowQrCode && <CameraPWA  />}
-      {isShowQrCode && <Test />}
+      <button onClick={() => setIsShowQrCode(!isShowQrCode)}>
+        Mostre QrCode
+      </button>
+      <h3>QR-CODE : {qrCode}</h3>
+      {!isShowQrCode && <CameraPWA />}
+      {isShowQrCode && (
+        <Html5QrcodePlugin
+          fps={10}
+          qrbox={250}
+          disableFlip={false}
+          qrCodeSuccessCallback={onNewScanResult}
+        />
+      )}
     </>
   );
 }
--- src/pages/QrScanner.tsx
+++ src/pages/QrScanner.tsx
@@ -1,12 +1,73 @@
-import { Scanner } from "@yudiel/react-qr-scanner";
-import { useState } from "react";
+import { Html5QrcodeScanner } from "html5-qrcode";
+import { useEffect } from "react";
 
-export const Test = () => {
-  const [text, setText] = useState('Not Found');
-  return (
-    <>
-      <h3>{text}</h3>
-      <Scanner onScan={(result) => setText(result[0].rawValue)} />;
-    </>
-  );
+const qrcodeRegionId = "html5qr-code-full-region";
+
+// Creates the configuration object for Html5QrcodeScanner.
+// eslint-disable-next-line
+// @ts-ignore
+const createConfig = (props) => {
+  // eslint-disable-next-line
+  // @ts-ignore
+  const config = {};
+
+  if (props.fps) {
+    // eslint-disable-next-line
+    // @ts-ignore
+    config.fps = props.fps;
+  }
+  if (props.qrbox) {
+    // eslint-disable-next-line
+    // @ts-ignore
+    config.qrbox = props.qrbox;
+  }
+  if (props.aspectRatio) {
+    // eslint-disable-next-line
+    // @ts-ignore
+    config.aspectRatio = props.aspectRatio;
+  }
+  if (props.disableFlip !== undefined) {
+    // eslint-disable-next-line
+    // @ts-ignore
+    config.disableFlip = props.disableFlip;
+  }
+
+  return config;
 };
+// eslint-disable-next-line
+// @ts-ignore
+const Html5QrcodePlugin = (props) => {
+  useEffect(() => {
+    // when component mounts
+    const config = createConfig(props);
+    const verbose = props.verbose === true;
+    // Suceess callback is required.
+    if (!props.qrCodeSuccessCallback) {
+      throw "qrCodeSuccessCallback is required callback.";
+    }
+    // eslint-disable-next-line
+    // @ts-ignore
+    const html5QrcodeScanner = new Html5QrcodeScanner(
+      qrcodeRegionId,
+      // eslint-disable-next-line
+      // @ts-ignore
+      config,
+      verbose
+    );
+    html5QrcodeScanner.render(
+      props.qrCodeSuccessCallback,
+      props.qrCodeErrorCallback
+    );
+
+    // cleanup function when component will unmount
+    return () => {
+      html5QrcodeScanner.clear().catch((error) => {
+        console.error("Failed to clear html5QrcodeScanner. ", error);
+      });
+    };
+  }, []);
+
+  return <div id={qrcodeRegionId} />;
+};
+
+export default Html5QrcodePlugin;

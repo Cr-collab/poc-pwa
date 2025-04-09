280358b09ae8b1436b9fe966dfe38302cb651aed
Cr-collab <cristiccorrea@gmail.com>
2025-04-09 16:16:22 -0300

    test 200

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
+import { useState } from "react";
 import "./App.css";
 import CameraPWA from "./pages/Camera.tsx";
 import { Test } from "./pages/QrScanner.tsx";
 import { GalleryAccess } from "./pages/TakeTheGalery.tsx";
 
 function App() {
+  const [isShowQrCode, setIsShowQrCode] = useState(false); 
   return (
     <>
       <GalleryAccess />
-      <CameraPWA />
-      <Test />
+      <button onClick={() => setIsShowQrCode(!isShowQrCode)}> Mostre QrCode</button>
+      {!isShowQrCode && <CameraPWA />}
+      {isShowQrCode && <Test />}
     </>
   );
 }

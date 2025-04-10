272f6345e2cc99bb11d9774ce81f10a26158b6bd
Cr-collab <cristiccorrea@gmail.com>
2025-04-10 07:17:16 -0300

    bbb

--- src/pages/QrScanner.tsx
+++ src/pages/QrScanner.tsx
@@ -3,22 +3,18 @@ import { Scanner } from "@yudiel/react-qr-scanner";
 // eslint-disable-next-line
 // @ts-ignore
 const QRScanner = ({ onScan }) => {
-
   return (
     <div>
-      {scanning && (
-        <>
-          <Scanner
-            onScan={onScan}
-            constraints={{
-              facingMode: "environment",
-              width: { ideal: 1280 },
-              height: { ideal: 720 },
-            }}
-          />
-        </>
-      )}
-      <button onClick={() => setScanning(true)}>Iniciar Scanner</button>
+      <>
+        <Scanner
+          onScan={onScan}
+          constraints={{
+            facingMode: "environment",
+            width: { ideal: 1280 },
+            height: { ideal: 720 },
+          }}
+        />
+      </>
     </div>
   );
 };

2fc5427a96907554100283c52ecde99d83d5fbb9
Cr-collab <cristiccorrea@gmail.com>
2025-04-10 07:16:34 -0300

    aa

--- src/pages/QrScanner.tsx
+++ src/pages/QrScanner.tsx
@@ -1,63 +1,21 @@
 import { Scanner } from "@yudiel/react-qr-scanner";
-import { useEffect, useRef, useState } from "react";
-import { toast } from "react-toastify";
+
 // eslint-disable-next-line
 // @ts-ignore
 const QRScanner = ({ onScan }) => {
-  const [scanning, setScanning] = useState(true);
-  const videoRef = useRef(null);
-
-  const [isIos, setIsIos] = useState(false);
-
-  const startCamera = async () => {
-    try {
-      const stream = await navigator.mediaDevices.getUserMedia({
-        video: {
-          facingMode: "environment", // Câmera traseira
-          width: { ideal: 1280 },
-          height: { ideal: 720 },
-        },
-      });
-
-      if (videoRef.current) {
-        // eslint-disable-next-line
-        // @ts-ignore
-        videoRef.current.srcObject = stream;
-      }
-    } catch (err) {
-      console.error("Erro na câmera:", err);
-      toast.error(
-        isIos
-          ? "No iOS, instale este PWA para acessar a câmera!"
-          : "Permissão da câmera negada"
-      );
-    }
-  };
-
-  const stopCamera = () => {
-    // eslint-disable-next-line
-    // @ts-ignore
-    if (videoRef.current?.srcObject) {
-      // eslint-disable-next-line
-      // @ts-ignore
-      videoRef.current.srcObject.getTracks().forEach((track) => track.stop());
-    }
-  };
-
-  useEffect(() => {
-    setIsIos(/iPad|iPhone|iPod/.test(navigator.userAgent));
-  }, []);
-
-  useEffect(() => {
-    startCamera();
-    return () => stopCamera();
-  }, []);
 
   return (
     <div>
       {scanning && (
         <>
-          <Scanner onScan={onScan} />
+          <Scanner
+            onScan={onScan}
+            constraints={{
+              facingMode: "environment",
+              width: { ideal: 1280 },
+              height: { ideal: 720 },
+            }}
+          />
         </>
       )}
       <button onClick={() => setScanning(true)}>Iniciar Scanner</button>
